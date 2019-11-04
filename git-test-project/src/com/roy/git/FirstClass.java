5543252549bf6fa06d0dfc8f11ab87ecdd04d207
royldh <591052734@qq.com>
2019-11-04 16:27:49 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/roy/git/FirstClass.java
@@ -0,0 +1,21 @@
+package com.roy.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+        System.out.println("heelo git project");
+    }
+    public void test(){
+        int i = 0;
+        i += 10;
+
+    }
+}
